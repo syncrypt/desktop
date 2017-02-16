600e2e939576dee5206d42a51990b9f0f62d80b7
Christopher Bertels <bakkdoor@flasht.de>
2017-02-17 00:26:37 +0100

    Use 127.0.0.1 for API url

--- src/Main.elm
+++ src/Main.elm
@@ -34,7 +34,7 @@ init =
 initialModel : Model
 initialModel =
     { config =
-        { apiUrl = "http://localhost:28080/v1/"
+        { apiUrl = "http://127.0.0.1:28080/v1/"
         , apiAuthToken =
             -- set this to your actual api auth token
             "my API token here"
