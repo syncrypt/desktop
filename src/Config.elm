3f96178b435f7fdbcf2727ca6b4c87ce6644b4b2
Christopher Bertels <bakkdoor@flasht.de>
2017-02-18 20:23:24 +0100

    Move init config code into Config.initialConfig

--- src/Config.elm
+++ src/Config.elm
@@ -8,3 +8,13 @@ type alias Config =
     , apiAuthToken : String
     , updateInterval : Time
     }
+
+
+initialConfig : Config
+initialConfig =
+    { updateInterval = 5000
+    , apiUrl = "http://127.0.0.1:28080/v1/"
+    , apiAuthToken =
+        -- set this to your actual api auth token
+        "my API token here"
+    }
--- src/Main.elm
+++ src/Main.elm
@@ -33,13 +33,7 @@ init =
 
 initialModel : Model
 initialModel =
-    { config =
-        { updateInterval = 5000
-        , apiUrl = "http://127.0.0.1:28080/v1/"
-        , apiAuthToken =
-            -- set this to your actual api auth token
-            "my API token here"
-        }
+    { config = Config.initialConfig
     , vaults = []
     , flyingVaults = []
     , state = LoadingVaults
