f8dfaa2db0fa0395f06c6cba4117fda363e9b7a1
Hannes Gräuler <graeuler@geoplex.de>
2017-07-29 16:23:10 +0200

    Receive userKeyState via API

---
+++ src/Syncrypt/Stats.elm
@@ -0,0 +1,19 @@
+module Syncrypt.Stats exposing (..)
+
+
+type KeyState
+    = Uninitialized
+    | Initializing
+    | Initialized
+
+
+type alias Stats =
+    { stats : Int
+    , downloads : Int
+    , uploads : Int
+    , userKeyState : KeyState
+    , totalSlots : Int
+    , busySlots : Int
+    , idleSlots : Int
+    , closedSlots : Int
+    }
