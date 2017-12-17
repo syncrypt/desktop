324d40f1852818b704664e1aebc1930e6c378144
Christopher Bertels <bakkdoor@flasht.de>
2017-12-17 21:19:07 +0100

    Add basic system tray icon

--- static/main.js
+++ static/main.js
@@ -1,3 +1,5 @@
+import { create } from 'domain';
+
 'use strict'
 const electron = require('electron')
 
@@ -7,24 +9,28 @@ const BrowserWindow = electron.BrowserWindow // This is a Module that creates wi
 const Path = require('path')
 const ChildProcess = require('child_process')
 const FileSystem = require('fs')
+const Tray = electron.Tray
 
-var mainWindow; // saves a global reference to mainWindow so it doesn't get garbage collected
+var mainWindow = null // saves a global reference to mainWindow so it doesn't get garbage collected
+var systemTray = null
+var daemon = null
 
 const appPath = Path.dirname(app.getAppPath());
-var daemonPath = Path.join(appPath, "app", "syncrypt", "syncrypt_daemon");
+let daemonPath = Path.join(appPath, "app", "syncrypt", "syncrypt_daemon");
 
 if (process.env.NODE_ENV === "development") {
-  daemonPath = "client/syncrypt_daemon"
+  daemonPath = Path.join("client", "syncrypt_daemon")
 }
 
 
 // Launch the Syncrypt daemon, if it exists
 function launchDaemon() {
-
-  if (process.platform == 'win32') daemonPath += '.exe';
+  if (process.platform == 'win32' && !daemonPath.includes(".exe")) {
+    daemonPath += '.exe';
+  }
 
   if (FileSystem.existsSync(daemonPath)) {
-    const daemon = ChildProcess.spawn(daemonPath);
+    daemon = ChildProcess.spawn(daemonPath)
 
     daemon.on('data', (data) => {
       console.log("daemon: ", data)
@@ -32,17 +38,36 @@ function launchDaemon() {
 
     daemon.on('close', (code) => {
       const msg = `child process exited with code ${code}`
-      console.log(msg);
-    });
-  }
-  else {
+      console.log(msg)
+    })
+  } else {
     console.warn('Did not start the daemon, because the following path does not ' +
       `exist: ${daemonPath}`)
   }
 }
 
+
+function restartDaemon() {
+  console.log("Daemon restart requested")
+  stopDaemon()
+  launchDaemon()
+}
+
+function stopDaemon() {
+  if (daemon) {
+    console.log("Stopping daemon")
+    daemon.kill()
+    daemon = null
+  }
+}
+
 // This will create our app window, no surprise there
 function createWindow() {
+  if (mainWindow) {
+    mainWindow.focus()
+    return
+  }
+
   var width = 832
   if (process.platform == 'win32') {
     width = 866
@@ -86,17 +111,39 @@ function createWindow() {
   })
 }
 
+function quitAll() {
+  console.log("Quitting Syncrypt Desktop")
+  stopDaemon()
+  if (mainWindow) {
+    mainWindow.close()
+  }
+  app.quit()
+}
+
+function createTray() {
+  systemTray = new Tray("assets/vault_tray_icon.png")
+  const contextMenu = Menu.buildFromTemplate([
+    { label: "Open Syncrypt Desktop", type: "normal", click: createWindow },
+    { label: "Restart Daemon", type: "normal", click: restartDaemon },
+    { label: "Stop Daemon", type: "normal", click: stopDaemon },
+    { label: "Quit", type: "normal", click: quitAll },
+  ])
+  systemTray.setToolTip("Syncrypt Desktop")
+  systemTray.setContextMenu(contextMenu)
+}
+
 // called when electron has initialized
 app.on('ready', () => {
   launchDaemon()
   createWindow()
+  createTray()
 })
 
 /* Mac Specific things */
 
 // when you close all the windows on a non-mac OS it quits the app
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') { app.quit() }
+  // if (process.platform !== 'darwin') { app.quit() }
 })
 
 // if there is no mainWindow it creates one (like when you click the dock icon)
