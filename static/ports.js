1f2e8d36ed48f6897d8ed86b0022850d438ab516
Christopher Bertels <bakkdoor@flasht.de>
2018-03-17 15:24:01 +0100

    Use const over var in ports.js

--- static/ports.js
+++ static/ports.js
@@ -7,7 +7,10 @@ const DaemonConfig = expandHomeDir('~/.config/syncrypt/config');
 const Electron = require('electron');
 const File = require('file');
 
-var readAuthToken = function (resolve, reject) {
+const mainContainer = window.document.getElementById("Root");
+var elmApp = null;
+
+const readAuthToken = function (resolve, reject) {
   Fs.readFile(DaemonConfig, 'utf-8', function (err, data) {
     if (err) {
       reject();
@@ -23,11 +26,8 @@ var readAuthToken = function (resolve, reject) {
   });
 }
 
-// TODO: get this from syncrypt daemon config file
-var mainContainer = window.document.getElementById("Root");
-var elmApp = null;
 
-var openFolderDialog = function (vaultId) {
+const openFolderDialog = function (vaultId) {
   var folders = Electron.remote.dialog.showOpenDialog({ properties: ["openDirectory", "createDirectory"] });
   if (folders && folders.length == 1) {
     var folderPath = folders[0].split(Path.sep);
@@ -35,7 +35,7 @@ var openFolderDialog = function (vaultId) {
   }
 }
 
-var openIconFileDialog = function (tag) {
+const openIconFileDialog = function (tag) {
   Electron.remote.dialog.showOpenDialog({
     properties: ["openFile"],
     title: "Select icon for vault",
@@ -53,7 +53,7 @@ var openIconFileDialog = function (tag) {
   });
 }
 
-var openExportFileDialog = function ([tag, buttonLabel]) {
+const openExportFileDialog = function ([tag, buttonLabel]) {
   Electron.remote.dialog.showSaveDialog({
     title: "Select file to export Vault key bundle to",
     buttonLabel: buttonLabel,
@@ -65,7 +65,7 @@ var openExportFileDialog = function ([tag, buttonLabel]) {
 
 const IGNORE_FILES = [".DS_Store", ".vault"];
 
-var getFileList = function ([vaultId, rootPathList]) {
+const getFileList = function ([vaultId, rootPathList]) {
   var rootPath = rootPathList.join(Path.sep)
   File.walk(rootPath, function (_, dirPath, dirs, files) {
     var path = Path.relative(rootPath, dirPath).split(Path.sep);
@@ -87,7 +87,7 @@ var getFileList = function ([vaultId, rootPathList]) {
   })
 }
 
-var isVaultPath = function (path) {
+const isVaultPath = function (path) {
   for (var i = 0; i < path.length; i++) {
     if (path[i] == ".vault") {
       return true;
@@ -96,14 +96,14 @@ var isVaultPath = function (path) {
   return false;
 }
 
-var focusOn = function (id) {
+const focusOn = function (id) {
   var elem = document.getElementById(id);
   if (elem) {
     elem.focus();
   }
 }
 
-var openVaultFolder = function (path) {
+const openVaultFolder = function (path) {
   console.log("open folder:", path);
   if (process.platform === 'darwin') {
     Electron.shell.openExternal("file://" + path);
@@ -112,7 +112,7 @@ var openVaultFolder = function (path) {
   }
 }
 
-var addEmailToCompletionList = function (email) {
+const addEmailToCompletionList = function (email) {
   var emails = Array(localStorage["EmailCompletionList"])
 
   if (emails.includes(email)) {
@@ -123,7 +123,7 @@ var addEmailToCompletionList = function (email) {
   localStorage["EmailCompletionList"] = emails
 }
 
-var updateEmailCompletionList = function () {
+const updateEmailCompletionList = function () {
   var emails = localStorage["EmailCompletionList"]
   if (emails) {
     elmApp.ports.getEmailCompletionList.send(Array(emails))
@@ -148,7 +148,7 @@ const openUserKeyExportFileDialog = (buttonLabel) => {
   });
 }
 
-var setupElmApp = function (daemonApiToken) {
+const setupElmApp = function (daemonApiToken) {
   elmApp = Elm.Main.embed(mainContainer, {
     apiAuthToken: daemonApiToken,
     apiUrl: "http://127.0.0.1:28080/v1/",
